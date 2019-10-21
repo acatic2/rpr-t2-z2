b71aaab017bddeca44afd2a56851417015554c23
acatic2 <acatic2@etf.unsa.ba>
2019-10-21 16:13:04 +0200

    Dodan dio metoda

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -2,5 +2,31 @@ package ba.unsa.etf.rpr.tutorijal02;
 
 public class Interval {
 
+private double pocetnaTacka;
+private double krajnjaTacka;
+private boolean pripadaIntervalu1;
+private boolean pripadaIntervalu2;
 
+    public Interval(double pocetnaTacka, double krajnjaTacka, boolean pripadaIntervalu1, boolean pripadaIntervalu2) {
+        if(pocetnaTacka>krajnjaTacka) throw  new IllegalArgumentException ("Parametar ne valja");
+        this.pocetnaTacka = pocetnaTacka;
+        this.krajnjaTacka = krajnjaTacka;
+        this.pripadaIntervalu1 = pripadaIntervalu1;
+        this.pripadaIntervalu2 = pripadaIntervalu2;
+    }
+    public Interval(){
+        pocetnaTacka=krajnjaTacka=0;
+        pripadaIntervalu1=pripadaIntervalu2=false;
+    }
+    boolean isNull(){
+        if(this.pocetnaTacka==this.krajnjaTacka) return  true;
+        return false;
+    }
+    boolean isIn(double tacka){
+        if(tacka>=pocetnaTacka && tacka<=krajnjaTacka) return  true;
+        return false;
+    }
+    public Interval intersect(Interval interval1, Interval interval2){
+        
+    }
 }
